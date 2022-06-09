c3a79080bd16c5e633504f42ba4c47221a1a050e
ArtemTsimbaliuk <timon9303@mail.ru>
2022-06-09 19:06:32 +0300

    first commit arrays homework

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -2,6 +2,8 @@ function compareArrays(arr1, arr2) {
   let result;
 
   // Ваш код
+  
+  result = arr1.every((i) => arr1.length === arr2.length && arr1[i] === arr2[i]);
 
   return result; // boolean
 }
@@ -10,6 +12,8 @@ function advancedFilter(arr) {
   let resultArr;
 
   // Ваш код
+
+  resultArr = arr.filter((i) => i > 0).filter((i) => i % 3 === 0).map((i) => i * 10);
 
   return resultArr; // array
 }
